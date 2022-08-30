92f53f26c4399e512baf297965f21b7f9a300360
aguerram <agurram2013@gmail.com>
2022-08-30 20:38:40 +0100

    save calculated md5 to files

--- src/main/java/com/github/aguerra/htmlurlchecksum/services/HtmlChecksumCalculator.java
+++ src/main/java/com/github/aguerra/htmlurlchecksum/services/HtmlChecksumCalculator.java
@@ -1,18 +1,25 @@
 package com.github.aguerra.htmlurlchecksum.services;
 
+import org.apache.hc.core5.net.URIBuilder;
 import org.jsoup.Jsoup;
+import org.jsoup.internal.StringUtil;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.math.BigInteger;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -34,38 +41,64 @@ public class HtmlChecksumCalculator {
 
     public void cssJsParser(File file) {
         try {
+            //don't judge this coding style, this is speedrun coding
             Document document = Jsoup.parse(file, null);
 
             Elements cssElements = document.select("link");
             Elements jsElements = document.select("script[src]");
             for (Element element : cssElements) {
-                logger.info("Found " + element);
-                var newLink = this.calculateCheckSum(element, "href");
-                logger.info("Generated link " + newLink);
-                element.attr("href", newLink);
+                processElement(element, "href");
             }
 
             for (Element element : jsElements) {
-                logger.info("Found " + element);
-                var newLink = this.calculateCheckSum(element, "src");
-                logger.info("Generated link " + newLink);
-                element.attr("src", newLink);
+                processElement(element, "src");
             }
+
+            BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file));
+
+            byte[] htmlBytes = document.html().getBytes(StandardCharsets.UTF_8);
+            outputStream.write(htmlBytes, 0, htmlBytes.length);
+            outputStream.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public String calculateCheckSum(Element element, String linkAttr) {
+    private void processElement(Element element, String linkAttr) throws URISyntaxException {
+        logger.info("Found " + element);
+        var link = element.attr(linkAttr);
+        //if link doesn't exist, do nothing
+        if (StringUtil.isBlank(link)) {
+            logger.info(element + " doesn't have an href or src attribute or it's null");
+            return;
+        }
+        URIBuilder builder = new URIBuilder(link);
+        //if the link contains already a md5 ignore it
+        if (builder.getQueryParams().size() > 0 && !StringUtil.isBlank(
+                Optional.ofNullable(builder.getFirstQueryParam("md5"))
+                        .map(String::valueOf)
+                        .orElse("")
+        )) {
+            logger.info(element + " has already md5 hash calculated");
+            return;
+        }
+        logger.info("Calculating md5 hash for element " + element);
+
+        var md5 = this.calculateCheckSum(element, linkAttr);
+        builder.addParameter("md5", md5);
+        element.attr(linkAttr, builder.toString());
+    }
+
+    private String calculateCheckSum(Element element, String linkAttr) {
         String link = element.attr(linkAttr);
         if (link == null) return null;
         try {
             String md5 = this.hashFile(link, "MD5");
-            return link + "?md5=" + md5;
+            return md5;
         } catch (Exception e) {
             logger.log(Level.WARNING, e.getMessage());
         }
-        return link;
+        return "";
     }
 
     private String hashFile(String link, String algorithm) {
